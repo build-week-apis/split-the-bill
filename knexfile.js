bf20e034c9d75ef3e53b58a0ac7f16053966b625
Adam Hinckley <adamhinckley@mac.com>
2019-08-28 17:55:22 -0600

    updates knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,36 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/splitthebill.db3',
+      filename: "./data/splitthebill.db3"
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds"
     },
     // needed to use foreign keys
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
+    client: "pg",
+    connection: process.env.DATABASE_URL + "?ssl=true",
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
-  },
+      directory: "./data/seeds"
+    }
+  }
 };
