196f3f07946a75223a0265af5d5f9ec2f8334c68
Adam Hinckley <adamhinckley@mac.com>
2019-08-28 15:23:20 -0600

    removes requirement for split sum

--- api/middleware/validate-middleware.js
+++ api/middleware/validate-middleware.js
@@ -1,6 +1,6 @@
-const Users = require('../users/user-model.js');
-const Bills = require('../bills/bill-model.js');
-const Notifications = require('../notifications/notification-model.js');
+const Users = require("../users/user-model.js");
+const Bills = require("../bills/bill-model.js");
+const Notifications = require("../notifications/notification-model.js");
 
 module.exports = {
   validateUserId,
@@ -8,21 +8,20 @@ module.exports = {
   validateBill,
   validateBillId,
   validateNotification,
-  validateNotificationId,
+  validateNotificationId
 };
 
 function validateUser(req, res, next) {
   const {
     body,
-    body: { email, firstname, lastname },
+    body: { email, firstname, lastname }
   } = req;
 
   if (!body) {
-    res.status(400).json({ warning: 'Missing user data entirely.' });
+    res.status(400).json({ warning: "Missing user data entirely." });
   } else if (!email || !firstname || !lastname) {
     res.status(400).json({
-      warning:
-        'Missing required email or firstname or lastname information for an user.',
+      warning: "Missing required email or firstname or lastname information for an user."
     });
   } else {
     next();
@@ -32,34 +31,32 @@ function validateUser(req, res, next) {
 async function validateUserId(req, res, next) {
   try {
     const {
-      params: { id },
+      params: { id }
     } = req;
 
     const user = await Users.findById(id);
     user
       ? ((req.user = user), next())
       : res.status(404).json({
-          info: `The user with the id ${id} was not found during validation.`,
+          info: `The user with the id ${id} was not found during validation.`
         });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: 'An error occurred during validation of the user.' });
+    res.status(500).json({ error: "An error occurred during validation of the user." });
   }
 }
 
 function validateBill(req, res, next) {
   const {
     body,
-    body: { split_sum, split_people_count, user_id },
+    body: { split_sum, split_people_count, user_id }
   } = req;
 
   if (!body) {
-    res.status(400).json({ warning: 'Missing bill data entirely.' });
+    res.status(400).json({ warning: "Missing bill data entirely." });
   } else if (!split_sum || !split_people_count || !user_id) {
     res.status(400).json({
       warning:
-        'Missing required split_sum or split_people_count or user_id information for a bill.',
+        "Missing required split_sum or split_people_count or user_id information for a bill."
     });
   } else {
     next();
@@ -69,34 +66,31 @@ function validateBill(req, res, next) {
 async function validateBillId(req, res, next) {
   try {
     const {
-      params: { id },
+      params: { id }
     } = req;
 
     const bill = await Bills.findById(id);
     bill
       ? ((req.bill = bill), next())
       : res.status(404).json({
-          info: `The bill with the id ${id} was not found during validation.`,
+          info: `The bill with the id ${id} was not found during validation.`
         });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: 'An error occurred during validating of a bill.' });
+    res.status(500).json({ error: "An error occurred during validating of a bill." });
   }
 }
 
 function validateNotification(req, res, next) {
   const {
     body,
-    body: { email, bill_id },
+    body: { email, bill_id }
   } = req;
 
   if (!body) {
-    res.status(400).json({ warning: 'Missing notification data entirely.' });
-  } else if (!split_sum || !email || !bill_id) {
+    res.status(400).json({ warning: "Missing notification data entirely." });
+  } else if (!email || !bill_id) {
     res.status(400).json({
-      warning:
-        'Missing required email or bill_id information for a notification.',
+      warning: "Missing required email or bill_id information for a notification."
     });
   } else {
     next();
@@ -106,7 +100,7 @@ function validateNotification(req, res, next) {
 async function validateNotificationId(req, res, next) {
   try {
     const {
-      params: { id },
+      params: { id }
     } = req;
 
     const notification = await Notifications.findById(id);
@@ -114,11 +108,11 @@ async function validateNotificationId(req, res, next) {
     bill
       ? ((req.notification = notification), next())
       : res.status(404).json({
-          info: `The notification with the id ${id} was not found during validation.`,
+          info: `The notification with the id ${id} was not found during validation.`
         });
   } catch (error) {
     res.status(500).json({
-      error: 'An error occurred during validation of a notification.',
+      error: "An error occurred during validation of a notification."
     });
   }
 }
